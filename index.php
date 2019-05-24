d9b879e02d5bf95ecd189f0d642320030b7180da
Felix Bello <felix.bello@becit.ch>
2019-05-24 23:53:01 +0200

    before columns layout

--- index.php
+++ index.php
@@ -56,25 +56,26 @@ $query = $down_datastore->query()->kind( $kind );
 ?>
 
 <html lang="en">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <title>ClCo WebApp Team A</title>
 <body>
-<h1>A Form in the Cloud</h1>
-<p>This is the Simple Webapplication for the Arbeitsauftrag AA2-a und Abgabe Präsentation</p>
+<h1 style="padding:1em;">A Form in the Cloud</h1>
+<p style="padding:1em;">This is the Simple Webapplication for the Arbeitsauftrag AA2-a und Abgabe Präsentation</p>
 
 <section>
-    <h2>Existing entries:</h2>
-    <ul>
+    <h2 style="padding:1em;">Existing entries:</h2>
+    <ul class="list-group">
 		<?php
 		foreach ( $down_datastore->runQuery( $down_datastore->query()->kind( $kind ) ) as $entity ) {
 			echo '<li><ul>';
-			echo '<li><strong>First name: </strong>' . $entity["first_name"] . '</li>';
-			echo '<li><strong>Last name: </strong>' . $entity["last_name"] . '</li>';
-			echo '<li><strong>Street: </strong>' . $entity["street"] . '</li>';
-			echo '<li><strong>Street No: </strong>' . $entity["street_no"] . '</li>';
-			echo '<li><strong>ZIP: </strong>' . $entity["zip"] . '</li>';
-			echo '<li><strong>City: </strong>' . $entity["city"] . '</li>';
-			echo '<li><strong>Email: </strong>' . $entity["email"] . '</li>';
-			echo '<li><strong>Created: </strong>' . $entity["created"] . '</li>';
+			echo '<li class="list-group-item"><strong>First name: </strong>' . $entity["first_name"] . '</li>';
+			echo '<li class="list-group-item"><strong>Last name: </strong>' . $entity["last_name"] . '</li>';
+			echo '<li class="list-group-item"><strong>Street: </strong>' . $entity["street"] . '</li>';
+			echo '<li class="list-group-item"><strong>Street No: </strong>' . $entity["street_no"] . '</li>';
+			echo '<li class="list-group-item"><strong>ZIP: </strong>' . $entity["zip"] . '</li>';
+			echo '<li class="list-group-item"><strong>City: </strong>' . $entity["city"] . '</li>';
+			echo '<li class="list-group-item"><strong>Email: </strong>' . $entity["email"] . '</li>';
+			echo '<li class="list-group-item"><strong>Created: </strong>' . $entity["created"] . '</li>';
 
 
 			echo '</ul></li>';
@@ -83,35 +84,53 @@ $query = $down_datastore->query()->kind( $kind );
     </ul>
 </section>
 
-<section>
-    <h2>Add your own address:</h2>
+<section style="padding:1em;">
+    <h2 style="padding:1em;">Add your own address:</h2>
     <form method="POST">
 
+		<div class="form-group" style="padding:1em;">
         <label for="first_name">First name</label>
-        <input id="first_name" name="first_name" type="text"/>
+        <input class="form-control" id="first_name" name="first_name" type="text"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="last_name">Last name</label>
-        <input id="last_name" name="last_name" type="text"/>
+        <input class="form-control" id="last_name" name="last_name" type="text"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="street">Street</label>
-        <input id="street" name="street" type="text"/>
+        <input class="form-control" id="street" name="street" type="text"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="street_no">Street no.</label>
-        <input id="street_no" name="street_no" type="text"/>
+        <input class="form-control" id="street_no" name="street_no" type="text"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="zip">ZIP</label>
-        <input id="zip" name="zip" type="text"/>
+        <input class="form-control" id="zip" name="zip" type="text"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="city">City</label>
-        <input id="city" name="city" type="text"/>
+        <input class="form-control" id="city" name="city" type="text"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="email">E-Mail</label>
-        <input id="email" name="email" type="email"/>
+        <input class="form-control" id="email" name="email" type="email"/>
+		</div>
 
+		<div class="form-group" style="padding:1em;">
         <label for="key">Key</label>
-        <input id="key" name="key" type="password"/>
+        <input class="form-control" id="key" name="key" type="password"/>
+		</div>
 
-        <input type="submit" value="Submit address"/>
+		<div class="form-group" style="padding:1em;">
+        <button type="submit" value="Submit address" class="btn btn-primary">Submit</button>
+		</div>
 
     </form>
 </section>
